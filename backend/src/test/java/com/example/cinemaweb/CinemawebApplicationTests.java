09464b6b31eb6cefcb141ec23496ed63fe6ee2f0
Nguyễn Quang Huy <nguyenquanghuylt2002@gmail.com>
2024-03-24 11:05:08 +0700

    Init project

---
+++ backend/src/test/java/com/example/cinemaweb/CinemawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
